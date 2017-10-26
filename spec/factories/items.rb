63416d03136b0696971d5df06e3d83683838b7cf
Evan Kaplan <evnkpln@gmail.com>
2017-10-26 17:12:01 -0400

    Add User List and Item models. Configure secure passwords.

---
+++ spec/factories/items.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :item do
+    name "MyString"
+    body "MyString"
+    list nil
+  end
+end
---
+++ spec/factories/lists.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :list do
+    name "MyString"
+    user nil
+    private false
+  end
+end
