8b54770669ab2f34298a32a33654718e9ec46de9
Evan Kaplan <evnkpln@gmail.com>
2017-10-27 05:53:06 -0400

    Omit authentication for user creation

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::UsersController < ApiController
-  before_action :authenticated?
+  before_action :authenticated?, except: [:create]
 
   def index
     render json: User.all, each_serializer: UserSerializer
