001ab753c137374a0f27e91251a6b0bf01ac7aca
Evan Kaplan <evnkpln@gmail.com>
2017-10-27 07:40:26 -0400

    Add List and Item create actions

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ActiveRecord::Base
   belongs_to :list
+  validates :name, length: { minimum: 1, maximum: 20 }, presence: true
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,6 @@
 class List < ActiveRecord::Base
   belongs_to :user
   has_many :items
+
+  validates :name, length: { minimum: 1, maximum: 50 }, presence: true
 end
