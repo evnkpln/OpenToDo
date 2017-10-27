75c4414409cfe892bd088ebc4c9aa2e324885645
Evan Kaplan <evnkpln@gmail.com>
2017-10-27 07:52:40 -0400

    Add delete action to all controllers.

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ActiveRecord::Base
   belongs_to :user
-  has_many :items
+  has_many :items, dependent: :destroy
 
   validates :name, length: { minimum: 1, maximum: 50 }, presence: true
 end
