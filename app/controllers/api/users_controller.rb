fff0389f04dc53e7d3d01785d90f180f6dbe49d9
Evan Kaplan <evnkpln@gmail.com>
2017-10-27 05:49:43 -0400

    Implement user creation

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -4,4 +4,18 @@ class Api::UsersController < ApiController
   def index
     render json: User.all, each_serializer: UserSerializer
   end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :name)
+  end
 end
